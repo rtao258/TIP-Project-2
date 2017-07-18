cac5af365546362556ca28aa2852ab432a306436
rtao258 <rtao258@users.noreply.github.com>
2017-07-18 18:08:49 -0400

    bad code

---
+++ type_tools.py
@@ -0,0 +1,41 @@
+import sys
+
+from collections import Mapping, Container
+from sys import getsizeof
+
+
+def deep_getsizeof(o, ids):
+    """Find the memory footprint of a Python object
+
+    This is a recursive function that drills down a Python object graph
+    like a dictionary holding nested dictionaries with lists of lists
+    and tuples and sets.
+
+    The sys.getsizeof function does a shallow size of only. It counts each
+    object inside a container as pointer only regardless of how big it
+    really is.
+
+    :param o: the object
+    :param ids:
+    :return:
+    """
+    if id(o) in ids:
+        return 0
+
+    r = getsizeof(o)
+    ids.add(id(o))
+
+    if isinstance(o, str) or isinstance(0, unicode):
+        return r
+
+    if isinstance(o, Mapping):
+        return r + sum(deep_getsizeof(k, ids) + deep_getsizeof(v, ids) for k, v in o.iteritems())
+
+    if isinstance(o, Container):
+        return r + sum(deep_getsizeof(x, ids) for x in o)
+
+    return r
+
+while True:
+    value = int(input("\nEnter a value: "))
+    print(deep_getsizeof(type(value)))
